fa6fd3b432c4a7c361d0c0ae72c06517b0befa96
kopycki <pawel.kopycki@gmail.com>
2018-08-09 13:51:41 +0200

    boundes

--- src/App.js
+++ src/App.js
@@ -12,15 +12,16 @@ class App extends Component {
     temp:'',
     icon:'',
     content:'',
+    bounds:'',
     locations: [
-      {title:'Radomska 60', location: {lat:51.432822, lng:21.320922}, direction:'Radom'},
-      {title:'Radomska 69', location: {lat:51.432921, lng:21.318971}, direction:'Jedlnia-Letnisko'},
-      {title:'Siczki', location: {lat:51.437613, lng:21.30547}, direction:'Radom'},
-      {title:'Radomska 32', location: {lat:51.432071, lng:21.326588}, direction:'Radom'},
-      {title:'Piłsudski Square', location: {lat:51.430069, lng:21.327622}, direction:'Radom'},
-      {title:'Pawel`s home', location: {lat:51.434571, lng: 21.316791}},
-      {title:'Monument to the victims of the Nazi occupation', location: {lat:51.430406, lng: 21.32743}},
-
+      {type: 'bus stop', title:'Radomska 60', location: {lat:51.432822, lng:21.320922}, direction:'Radom'},
+      {type: 'bus stop', title:'Radomska 69', location: {lat:51.432921, lng:21.318971}, direction:'Jedlnia-Letnisko'},
+      {type: 'bus stop', title:'Siczki', location: {lat:51.437613, lng:21.30547}, direction:'Radom'},
+      {type: 'bus stop', title:'Radomska 32', location: {lat:51.432071, lng:21.326588}, direction:'Radom'},
+      {type: 'bus stop', title:'Piłsudski Square', location: {lat:51.430069, lng:21.327622}, direction:'Radom'},
+      {type:'place', title:'Pawel`s home', location: {lat:51.434571, lng: 21.316791}},
+      {type:'place', title:'Monument to the victims of the Nazi occupation', location: {lat:51.430406, lng: 21.32743}},
+      {type:'place', title:'Church of St Joseph', location: {lat:51.434173, lng: 21.327767}},
     ]
   }
   
@@ -69,7 +70,10 @@ class App extends Component {
 
  initMap = () => {
   //  let markers = this.state.markers
+  // let infoWindow = new google.maps.InfoWindow();
   const google = window.google || {};
+  let bounds = new google.maps.LatLngBounds();
+
   // google.maps = google.map || {};
   this.map = new google.maps.Map(document.getElementById('map'), {
     center: {lat:51.434571, lng: 21.316791},
@@ -78,29 +82,35 @@ class App extends Component {
   this.setState({map:this.map})
   // let pawel =   {lat:51.434571, lng: 21.316791};
   for(let location of this.state.locations){
-    console.log(location)
+    // console.log(location)
     let marker = new google.maps.Marker({
       position: location.location,
       map: this.map,
-      
       animation: google.maps.Animation.DROP,
-      title:location.title,
+      title:`${location.type.toUpperCase()}\n${location.title}`,
+      type: location.type,
       
     }) 
-    this.state.markers.push(marker)
+    this.state.markers.push(marker);
+    bounds.extend(marker.position)
 
   }
+
+  this.setState({bounds: bounds})
+
+  this.map.fitBounds(bounds)
   
   for(let marker of this.state.markers) {
-    marker.setMap(this.state.map)
+    // marker.setMap(this.state.map)
       marker.addListener('click', () => {
         this.showListing(marker)
       })
   }
-  // let infoWindow = new google.maps.InfoWindow({
+  // console.log(bounds)
+  
   //   content: "Tralala ldsasjfsa"
   // }) 
-  console.log(this.state.markers) 
+  // console.log(this.state.markers) 
 }
 
 showListing = (marker) => {
@@ -111,29 +121,61 @@ showListing = (marker) => {
     marker.setMap(this.state.map)
   }
 }
-        
-test = () => {
-   for(let marker of this.state.markers){
-     this.showListing(marker)
 
-   } 
-  // console.log(this.state.markers[0])
-    
-    
+show = (marker, type, bounds) => {
+  
+  if(marker.type === type) {
+    marker.setMap(this.state.map)
+    bounds.extend(marker.position)
+  }else{
+    marker.setMap(null)
+  }
 }
+        
+buses = () => {
+  const google = window.google || {};
+  let bounds = new google.maps.LatLngBounds();
+  for(let marker of this.state.markers){
+    this.show(marker, 'bus stop', bounds)
+  } 
+  this.state.map.fitBounds(bounds)   
+}
+
+places = () => {
+  const google = window.google || {};
+  let bounds = new google.maps.LatLngBounds();
+  console.log(bounds)
+  for(let marker of this.state.markers){
+    this.show(marker, 'place', bounds)
+    
+  }   
+    this.state.map.fitBounds(bounds)
+}
+
+
   render() {
     const { temp, icon } = this.state
     return (
       <div  className="App">
         <header className="App-header">    
           <h1 className="App-title">MY NEIGHBORHOOD</h1>
+          <div className="search-bar" >
+            <div onClick={this.buses} className="category">
+              BUS STOPS
+            </div>
+            <div onClick={this.places} className="category">
+              PLACES
+            </div>
+          </div>
         </header>
-            <div id="map"></div>
-            <Weather 
-              icon={icon}
-              temp={temp}
-            />
-            <button onClick={this.test} >Click</button>
+
+
+        <div id="map"></div>
+        <Weather 
+          icon={icon}
+          temp={temp}
+        />
+        
         
       </div>
     );
--- src/Weather.js
+++ src/Weather.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react'
 
 
 class Weather extends Component {
-
-    render () {
-        const { temp, icon} = this.props
-        return (
-            
-            <div className="weather">
-            <div>{temp} &#176;C</div>
-            {icon?<img src={icon} alt="weather`s icon" />:<span>...</span>}
-            <p>Powered by Openweathermap.org</p>
-            </div>
-        )
-    }
+  render() {
+    const { temp, icon } = this.props
+    return (
+      <div className="weather">
+        <div>{temp} &#176;C</div>
+        {icon ? <img src={icon} alt="weather`s icon" /> : <span>...</span>}
+        <p>Powered by Openweathermap.org</p>
+      </div>
+    )
+  }
 
 }
 
